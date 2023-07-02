10b965ecf3cfbc944736d81b2711c20f267b3f0a
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2023-07-02 17:10:42 +0900

    [Add] 簡易的にフォーム作成

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1 className='title'>Todoリスト</h1>
+
+      <form onSubmit={() => {}} className='form'>
+        <input type="text" onChange={() => {}} className='form__input' />
+
+        <input type="submit" value="作成" className='form__submit' />
+      </form>
     </div>
   );
 }

bb35a9a349833cc3c92dd27218a4227ef1454bcb
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2023-07-02 17:18:59 +0900

    [Add] handleChange関数作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  }
+
   return (
     <div className="App">
       <h1 className='title'>Todoリスト</h1>
 
       <form onSubmit={() => {}} className='form'>
-        <input type="text" onChange={() => {}} className='form__input' />
+        <input type="text" onChange={(e) => handleChange(e)} className='form__input' />
 
         <input type="submit" value="作成" className='form__submit' />
       </form>
